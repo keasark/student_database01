cdefcac9358d3115825932ae17dfee419343da87
keasark <maixtran.vn@gmail.com>
2023-01-19 05:24:02 +0000

    Save progress: 560.1

--- insert_data.sh
+++ insert_data.sh
@@ -2,6 +2,8 @@
 
 # Script to insert data from courses.csv and students.csv into students database
 
+PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
+
 cat courses.csv | while IFS="," read MAJOR COURSE
 do
   #get major_id
@@ -13,5 +15,6 @@ do
   #if not found
   #insert course
   #get new course_id
+
   #insert into majors_courses
 done
