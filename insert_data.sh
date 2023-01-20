a077db5b0a69be3568d9f5087b311d08bac442ed
keasark <maixtran.vn@gmail.com>
2023-01-20 09:47:36 +0000

    Save progress: 1030.1

--- insert_data.sh
+++ insert_data.sh
@@ -56,10 +56,11 @@ cat students_test.csv | while IFS="," read FIRST LAST MAJOR GPA
 do
   if [[ $FIRST != "first_name" ]]
   then
-      # get major_id
-  # if not found
-  # set to null
-  # insert student
+    # get major_id
+    MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+    # if not found
+    # set to null
+    # insert student
   fi
 
 done
