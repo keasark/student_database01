e22bc6574bbedcaee7c1e31784792675e7a3afb2
keasark <maixtran.vn@gmail.com>
2023-01-20 05:45:58 +0000

    Save progress: 840.1

--- insert_data.sh
+++ insert_data.sh
@@ -6,7 +6,8 @@ PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only
 
 cat courses_test.csv | while IFS="," read MAJOR COURSE
 do
-  if [[ $MAJOR != major ]]
+  # Update major
+  if [[ $MAJOR != "major" ]]
   then
     # get major_id
     MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
@@ -14,24 +15,29 @@ do
     # if not found
     if [[ -z $MAJOR_ID ]]
     then
-    # insert major
+      # insert major
       INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR')")
       if [[ $INSERT_MAJOR_RESULT == "INSERT 0 1" ]]
       then
         echo Inserted into majors, $MAJOR
       fi
-    # get new major_id
-        MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+
+      # get new major_id
+      MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
     fi
+
+    if [[ $COURSE != "course" ]]
+    then
+    # get course_id
+      COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")
+
+    # if not found
+
+    # insert course
+
+    # get new course_id
+
+    # insert into majors_courses
+
   fi
-  # get course_id
-
-  # if not found
-
-  # insert course
-
-  # get new course_id
-
-  # insert into majors_courses
-
 done
